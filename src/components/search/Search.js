e16859e77bb2ca5d5d10e1a76126bea3d0fe99e8
habibmustafa <hebibullahmustafazade@gmail.com>
2022-07-14 23:23:37 +0400

    add: openPopup

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
-import {IoSearchOutline} from "react-icons/io5"
+import { IoSearchOutline } from "react-icons/io5"
 import "./Search.css"
 
 export default class Search extends Component {
+  openPopup = () => {
+    this.props.openPopup();
+  }
+
   render() {
     return (
       <div className='search-create'>
         <div className="serach__input-container">
-          <button className='search__submit'><IoSearchOutline  /></button>
-          <input className='search__input' type="text" placeholder='Search Categories'/>
+          <button className='search__submit'><IoSearchOutline /></button>
+          <input className='search__input' type="text" placeholder='Search Categories' />
         </div>
-        <button className='create__btn'>Create</button>
+        <button onClick={this.openPopup} className='create__btn'>Create</button>
       </div>
     )
   }
