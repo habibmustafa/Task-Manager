38ab74c28285327752672205e9cbfafa780cd5e8
habibmustafa <hebibullahmustafazade@gmail.com>
2022-07-14 23:25:22 +0400

    update: App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import React, { Component } from 'react';
-import Aside from './components/aside/Aside';
-import "./App.css"
-import Search from './components/search/Search';
-import Filter from './components/filter/Filter';
+import React, { Component } from "react";
+import Aside from "./components/aside/Aside";
+import "./App.css";
+import Search from "./components/search/Search";
+import Filter from "./components/filter/Filter";
+import TaskItems from "./components/taskItems/TaskItems";
+import CreateTask from "./components/createTask/CreateTask";
 
 class App extends Component {
-    render() {
-        return (
-            <div className='container'>
-                <aside className='aside'>
-                    <Aside />
-                </aside>
-                <main className="main">
-                    <Search />
-                    <Filter />
-                </main>
-            </div>
-        );
-    }
+  state = {
+    isPopupOpen: true,
+  };
+
+  togglePopup = () => {
+    this.setState({
+      isPopupOpen: !this.state.isPopupOpen
+    })
+  };
+
+  render() {
+    const { isPopupOpen } = this.state;
+
+    return (
+      <>
+        <div className="container">
+          <aside className="aside">
+            <Aside />
+          </aside>
+          <main className="main">
+            <Search openPopup={this.togglePopup}/>
+            <Filter />
+            <TaskItems />
+          </main>
+        </div>
+        {isPopupOpen ? (
+          <div className="popup">
+            <CreateTask closePopup={this.togglePopup} />
+          </div>
+        ) : null}
+      </>
+    );
+  }
 }
 
 export default App;
