17af51afcd73eccd0dbfbcfe052d2d929e0d3b9e
habibmustafa <hebibullahmustafazade@gmail.com>
2022-07-17 21:23:37 +0400

    add: complete and draft

--- src/components/taskItem/TaskItem.js
+++ src/components/taskItem/TaskItem.js
@@ -5,14 +5,44 @@ import TaskState from '../taskState/TaskState';
 import "./TaskItem.css"
 
 export default class TaskItem extends Component {
+
+  constructor(props){
+    super(props);
+    this.tasks = JSON.parse(localStorage.tasks);
+  }
+
+  state = {
+    taskState: this.props.state,
+  };
+  
+  toDraft = () => {
+    const draftItemIndex = this.tasks.findIndex(task => task.title === this.props.title);
+    this.tasks[draftItemIndex].state = "draft";
+    localStorage.setItem("tasks",  JSON.stringify(this.tasks));
+    
+    this.setState({
+      taskState: "draft",
+    });
+  };
+
+  toCompleted = () => {
+    const draftItemIndex = this.tasks.findIndex(task => task.title === this.props.title);
+    this.tasks[draftItemIndex].state = "completed";
+    localStorage.setItem("tasks",  JSON.stringify(this.tasks));
+
+    this.setState({
+      taskState: "completed",
+    });
+  };
+
   render() {
     return (
       <div className='task-item'>
-        <TaskState data={this.props.state}/>
+        <TaskState taskState={this.state.taskState} toDraft={(e) => this.toDraft(e)} toCompleted={this.toCompleted}/>
         <h3 className="task-item__title">{this.props.title}</h3>
         <p className="task-item__date">{this.props.date}</p>
         <p className="task-item__desc">{this.props.desc}</p>
-        <List data={this.props.list}/>
+        <List list={this.props.list}/>
         <Keywords />
       </div>
     )
